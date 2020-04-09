f4c3dec34e4d2a71ae1b6e896c9bd587d6c0b599
hollaa <alica.holla@gmail.com>
2020-04-09 18:07:56 +0200

    menu

--- script.js
+++ script.js
@@ -8,9 +8,36 @@ canvas.width=500;
 ctx.font = "30px Arial";
 ctx.fillText("Nasobilka", 10, 50);
 
-var mouseY=0;
-var mouseX=0;
+var gameWidth = canvas.width;
+var gameHeight = canvas.height;
 
+var startBtn={
+  x:157,
+  y:240,
+  width: 190,
+  height: 40,
+} // start button pozicie
+function getMousePos(canvas, event) {
+  var rect = canvas.getBoundingClientRect();
+  return {
+    x: event.clientX - rect.left,
+    y: event.clientY - rect.top
+  };
+}
+
+function isInside(pos, rect){
+  return pos.x > rect.x && pos.x < rect.x+rect.width && pos.y < rect.y+rect.heigth && pos.y > rect.y
+}
+
+canvas.addEventListener('click', function(evt) {
+  var mousePos = getMousePos(canvas, evt);
+  if (isInside(mousePos,startBtn)) {
+    
+    requestAnimationFrame(draw);
+  }
+}, false);
+
+//nacitavanie obrazkov
 let BgImage=document.createElement('image');
 let image6 = new Image;
 BgImage=image6;
@@ -41,7 +68,7 @@ let image4=new Image;
 appleImg=image4;
 appleImg.src='imgs/apple.png';
 
-class Box{
+class Box{ //hlava hadika
     constructor(){
         this.x=200;
         this.y=150;
@@ -54,6 +81,7 @@ class Box{
 
 let box=new Box();
 
+//vykreslovanie obrazkov
 function rpoint() {
     ctx.drawImage(rPoint,50,100,100,100);
   }
@@ -68,7 +96,19 @@ function apple() {
 
 function Menu(){
   ctx.drawImage(menuImage, 0, 0, 500, 500);
+  ctx.beginPath();
+  ctx.rect(157, 240, 190, 40);
+  ctx.fillStyle = '#FFFFFF';
+  ctx.fillStyle = 'rgba(225,225,225,0.5)';
+  ctx.fillRect(25,72,32,32);
+  ctx.fill();
+  ctx.lineWidth = 2;
+  ctx.strokeStyle = '#000000';
+  ctx.stroke();
+  ctx.closePath();
+  requestAnimationFrame(Menu);
 }
+requestAnimationFrame(Menu);
 
 function Bg(){
   ctx.drawImage(BgImage, 0, 0, 500, 500);
@@ -77,47 +117,34 @@ function Bg(){
 function showBox() {
     ctx.fillStyle = '#000';
     ctx.drawImage(box.image,box.x + box.vx,box.y + box.vy);
-    if((box.x-=3) <= 0){
-      box.x=500;
-    }
-    if(box.x==500){
-      box.y-=100;
-    }
-    if(box.x==500){
-      box.y+=100;
-    }
   }
 
-  function init(){
-    Menu();
-    document.addEventListener('click', mouseClicked, false);
-  }
-
-  function playGame(){
-    
-  }
-
-
-
-  function draw(){
+function draw(){
     ctx.clearRect(0,0,canvas.width,canvas.height);
     Bg();
-    ctx.font='30pt Pixel';
-    ctx.fillStyle = 'Black';
-    ctx.fillText('Násobilka s hadíkom Edom',30,100);
     rpoint();
     apple();
     wpoint();
     showBox();
-   
-    // Menu();
-  
+    Menu();
     requestAnimationFrame(draw);
   }  
-  requestAnimationFrame(draw);
 
-  function mouseClicked(e){
-  
+  function move(e){
+    if(e.keyCode == 39){
+      box.x += 5; 
+    }
+    if(e.keyCode == 37){
+      box.x -= 5; 
+    }
+    if(e.keyCode == 38){
+      box.y -= 5; 
+    }
+    if(e.keyCode == 40){
+      box.y += 5; 
+    }
+    
   }
+  document.onkeydown = move;
 
-
+ 
